e7f931373b1b3660160ee09387eb5e79ba398f38
Alex Mukha <44630121+alexmukha@users.noreply.github.com>
2019-08-04 00:21:06 -0700

    link test

--- js/secondary.js
+++ js/secondary.js
@@ -3,12 +3,7 @@ var ptitle=document.getElementsByName("location")[0].content;
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 console.log(ptitle); 
-gtag('js', new Date());
-gtag('config', 'UA-144720985-1');
-gtag('config', {
-    hitType: 'pageview',
-    title: ptitle,
-    page: location.pathname,
-    location: location.pathname
-  });
+gtag("js", new Date());
+gtag("config", "UA-144720985-1");
+gtag("send", '"pageview", location.pathname, "title", "page", "Location");
 };
